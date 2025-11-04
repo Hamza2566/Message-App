2a70ee4263c18c0a9a08ff93a007c08cf33a77ac
Hamza <xsharif2551@gmail.com>
2025-11-04 16:35:53 +0300

    made connection between frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-        
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 9500;
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
